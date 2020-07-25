5a9017d89ffdc38bd7ec356d1a07184d3fe4d0da
dani <gehr.danielle@gmail.com>
2020-07-25 10:33:05 -0400

    fixed error in api.js

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -7,11 +7,7 @@ export default {
   },
   // Gets the events with the given id
   getEvents: function(id) {
-<<<<<<< HEAD
     return axios.get("/api/events" + id);
-=======
-    return axios.get("/api/events" + "id");
->>>>>>> db34e857b5ba4f3c3b6d35436e5cc5ebe5c28801
   },
   // Deletes the events with the given id
   deleteEvents: function(findById) {
